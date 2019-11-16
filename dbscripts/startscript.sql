c37868e6cf3afc841676c7f8f576fbda64f1008e
Rafael Samorinha <rafaelsamorinha@gmail.com>
2019-11-16 19:47:36 +0000

    .

--- dbscripts/startscript.sql
+++ dbscripts/startscript.sql
@@ -8,39 +8,39 @@ CREATE TABLE IF NOT EXISTS Station(
     PRIMARY KEY (stationID)
 );
 
-CREATE TABLE IF NOT EXISTS Measurement(
-    measurementID INT NOT NULL AUTO_INCREMENT,
-    stationID INT NOT NULL,
-    time TIME NOT NULL,
-    date DATE NOT NULL,
-    GPSlatitude DECIMAL(3,8) NOT NULL,
-    GPSlongitude DECIMAL(3,8) NOT NULL,
-    ZoneLatitude FLOAT(3,2) NOT NULL,
-    ZoneLongitude FLOAT(3,2) NOT NULL,
-    NO2 INT,
-    CO INT,
-    CO2 INT,
-    TVOC INT,
-    Temperature FLOAT(2,1),
-    Humidity FLOAT(2,1),
-    PRIMARY KEY (measurementID),
-    FOREIGN KEY (stationID) REFERENCES Station(stationID),
-    FOREIGN KEY (ZoneLatitude, ZoneLongitude) REFERENCES Zone (ZoneLatitude, ZoneLongitude)
-);
-
 CREATE TABLE IF NOT EXISTS Zone(
-    ZoneLatitude FLOAT(3,2) NOT NULL,
-    ZoneLongitude FLOAT(3,2) NOT NULL,
+    ZoneLatitude FLOAT(5,2) NOT NULL,
+    ZoneLongitude FLOAT(5,2) NOT NULL,
     PRIMARY KEY (ZoneLatitude, ZoneLongitude)
 );
 
 CREATE TABLE IF NOT EXISTS Prediction(
-    ZoneLatitude FLOAT(3,2) NOT NULL,
-    ZoneLongitude FLOAT(3,2) NOT NULL,
+    ZoneLatitude FLOAT(5,2) NOT NULL,
+    ZoneLongitude FLOAT(5,2) NOT NULL,
     date DATE NOT NULL,
     NO2 INT,
     CO INT,
     CO2 INT,
     TVOC INT,
     PRIMARY KEY (ZoneLatitude, ZoneLongitude, date)
+);
+
+CREATE TABLE IF NOT EXISTS Measurement(
+    measurementID INT NOT NULL AUTO_INCREMENT,
+    stationID INT NOT NULL,
+    time TIME NOT NULL,
+    date DATE NOT NULL,
+    GPSlatitude FLOAT(11,8) NOT NULL,
+    GPSlongitude FLOAT(11,8) NOT NULL,
+    ZoneLatitude FLOAT(5,2) NOT NULL,
+    ZoneLongitude FLOAT(5,2) NOT NULL,
+    NO2 INT,
+    CO INT,
+    CO2 INT,
+    TVOC INT,
+    Temperature FLOAT(3,1),
+    Humidity FLOAT(3,1),
+    PRIMARY KEY (measurementID),
+    FOREIGN KEY (stationID) REFERENCES Station(stationID),
+    CONSTRAINT Fk_Measurement FOREIGN KEY (ZoneLatitude,ZoneLongitude) REFERENCES Zone(ZoneLatitude,ZoneLongitude)
 );
