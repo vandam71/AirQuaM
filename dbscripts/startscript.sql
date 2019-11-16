7b1ad282a5d32446ac81db08d69eaf5a8941cd9d
Rafael Samorinha <rafaelsamorinha@gmail.com>
2019-11-16 18:31:59 +0000

    .

--- dbscripts/startscript.sql
+++ dbscripts/startscript.sql
@@ -1,19 +1,20 @@
+DROP DATABASE airquam;
 CREATE DATABASE IF NOT EXISTS airquam;
-USE aiquam;
+USE airquam;
 
 CREATE TABLE IF NOT EXISTS Station(
     stationID INT NOT NULL AUTO_INCREMENT,
-    stationName CHAR(20) NOT NULL,
+    name CHAR(20) NOT NULL,
     PRIMARY KEY (stationID)
 );
 
 CREATE TABLE IF NOT EXISTS Measurement(
     measurementID INT NOT NULL AUTO_INCREMENT,
     stationID INT NOT NULL,
-    measurementTime TIME NOT NULL,
-    measurementDate DATE NOT NULL,
-    GPSlatitude FLOAT(3,8) NOT NULL,
-    GPSlongitude FLOAT(3,8) NOT NULL,
+    time TIME NOT NULL,
+    date DATE NOT NULL,
+    GPSlatitude DECIMAL(3,8) NOT NULL,
+    GPSlongitude DECIMAL(3,8) NOT NULL,
     ZoneLatitude FLOAT(3,2) NOT NULL,
     ZoneLongitude FLOAT(3,2) NOT NULL,
     NO2 INT,
@@ -23,24 +24,23 @@ CREATE TABLE IF NOT EXISTS Measurement(
     Temperature FLOAT(2,1),
     Humidity FLOAT(2,1),
     PRIMARY KEY (measurementID),
-    FOREIGN KEY (stationID)
-        REFERENCES Station(stationID)
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    FOREIGN KEY 
+    FOREIGN KEY (stationID) REFERENCES Station(stationID),
+    FOREIGN KEY (ZoneLatitude, ZoneLongitude) REFERENCES Zone (ZoneLatitude, ZoneLongitude)
 );
 
 CREATE TABLE IF NOT EXISTS Zone(
     ZoneLatitude FLOAT(3,2) NOT NULL,
-    ZoneLongitude FLOAT(3,2) NOT NULL
+    ZoneLongitude FLOAT(3,2) NOT NULL,
+    PRIMARY KEY (ZoneLatitude, ZoneLongitude)
 );
 
 CREATE TABLE IF NOT EXISTS Prediction(
     ZoneLatitude FLOAT(3,2) NOT NULL,
     ZoneLongitude FLOAT(3,2) NOT NULL,
-    predictionDate DATE NOT NULL,
+    date DATE NOT NULL,
     NO2 INT,
     CO INT,
-    CO2 INT
-    TVOC INT
+    CO2 INT,
+    TVOC INT,
+    PRIMARY KEY (ZoneLatitude, ZoneLongitude, date)
 );
