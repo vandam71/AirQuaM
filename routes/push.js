9c483353c64fa3a6f6ffc12f34cd658c12fd3537
Rafael Samorinha <rafaelsamorinha@gmail.com>
2019-11-18 16:24:00 +0000

    .

--- routes/push.js
+++ routes/push.js
@@ -8,10 +8,38 @@ router.get('/test', function (req, res, next) {
     res.render('dbtest');
 });
 
-router.post('/db', function (req, res) {
-    connection.query("INSERT INTO Zone (ZoneLatitude, ZoneLongitude) VALUES ('" + req.body.a + "', '" + req.body.b + "')", function(err, result){
-        if (err) throw err;
-        console.log("Inserted");
+router.post('/Zone', function (req, res) {
+    var total = 0;
+    var insert = 1;
+    connection.query("SELECT ZoneLatitude, ZoneLongitude FROM Zone", function (err, result, fields) {
+        if (err) {
+            console.error(err);
+        }
+        for (var i = 0; i<result.length; i++) {
+            if (result[i].ZoneLatitude == req.body.a) {
+                insert = 0;
+                break;
+            };
+            if (result[i].ZoneLongitude == req.body.b) {
+                insert = 0;
+                break;
+            }
+            insert = 1;
+        };
+        if (insert == 0) {
+        } else if (insert == 1) {
+            connection.query("INSERT INTO Zone (ZoneLatitude, ZoneLongitude) VALUES ('" + req.body.a + "', '" + req.body.b + "')", function (err, result) {
+                if (err) {
+                    console.error(err);
+                }
+            });
+        }
+    });
+    connection.query("SELECT * FROM Zone", function (err, result, fields) {
+        if (err) {
+            console.error(err);
+        }
+        res.send(result);
     });
 });
 
